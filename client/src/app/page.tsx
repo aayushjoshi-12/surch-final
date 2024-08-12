d0dbd816e09214c72f2375717770633b3e46731b
Aryan Yadav <yadavaryan466@gmail.com>
2024-08-12 12:12:28 +0530

    deploy-test: added axios credentials

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import Navbar from "@/components/global/navbar";
 
 import { SidebarDemo } from "./_components/sidebardemo";
+import axios from "axios";
 
 export default function Home() {
-
+  axios.defaults.withCredentials = true;
 
   return (
     <main>
