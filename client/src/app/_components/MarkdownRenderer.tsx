9d179146dd842c18f962eaf402ad8ecc456928b5
Aryan Yadav <yadavaryan466@gmail.com>
2024-08-12 11:33:07 +0530

    fix: client MarkdownRenderer error

--- client/src/app/_components/MarkdownRenderer.tsx
+++ client/src/app/_components/MarkdownRenderer.tsx
@@ -1,40 +1,44 @@
 // components/MarkdownRenderer.tsx
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import ReactSyntaxHighlighter, { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import React, { useRef, useMemo } from 'react';
+import ReactMarkdown, { Components } from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import { Components } from 'react-markdown/lib/ast-to-react';
 
 interface MarkdownRendererProps {
   markdown: string;
+  syntaxStyle?: any;
 }
 
-const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ markdown }) => {
-  return (
-    <ReactMarkdown
-      components={{
-        code({ node, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '');
-          return match ? (
-            <SyntaxHighlighter
-              style={atomDark as any}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {markdown}
-    </ReactMarkdown>
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
+  markdown,
+  syntaxStyle = atomDark,
+}) => {
+  const ref = useRef<SyntaxHighlighter>(null);
+
+  const components = useMemo<Components>(
+    () => ({
+      code({ node, className, children, ...props }: any) {
+        const match = /language-(\w+)/.exec(className || '');
+        const language = match ? match[1] : 'text'; // Default to 'text' if language is not found
+
+        return (
+          <SyntaxHighlighter
+            ref={ref}
+            style={syntaxStyle}
+            language={language}
+            PreTag="div"
+            {...props}
+          >
+            {String(children).replace(/\n$/, '')}
+          </SyntaxHighlighter>
+        );
+      },
+    }),
+    [syntaxStyle] // Recompute only if syntaxStyle changes
   );
+
+  return <ReactMarkdown components={components}>{markdown}</ReactMarkdown>;
 };
 
 export default MarkdownRenderer;
